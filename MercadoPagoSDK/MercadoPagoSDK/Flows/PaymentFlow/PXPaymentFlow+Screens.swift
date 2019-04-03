8e8a852022f566ee24c103dad6464daac249d478
Eden <eden.etorres@gmail.com>
2019-04-03 14:25:08 -0300

    Rename PXPaymentFlow+Screens

---
+++ MercadoPagoSDK/MercadoPagoSDK/Flows/PaymentFlow/PXPaymentFlow+Screens.swift
@@ -0,0 +1,26 @@
+//
+//  PXPaymentFlow+Screens.swift
+//  MercadoPagoSDK
+//
+//  Created by Eden Torres on 18/07/2018.
+//  Copyright © 2018 MercadoPago. All rights reserved.
+//
+
+import Foundation
+
+extension PXPaymentFlow {
+    internal func showPaymentProcessor(paymentProcessor: PXSplitPaymentProcessor?) {
+        guard let paymentProcessor = paymentProcessor else {
+            return
+        }
+
+        model.assignToCheckoutStore()
+
+        paymentProcessor.didReceive?(navigationHandler: PXPaymentProcessorNavigationHandler(flow: self))
+
+        if let paymentProcessorVC = paymentProcessor.paymentProcessorViewController() {
+            pxNavigationHandler.addDynamicView(viewController: paymentProcessorVC)
+            self.pxNavigationHandler.navigationController.pushViewController(paymentProcessorVC, animated: false)
+        }
+    }
+}

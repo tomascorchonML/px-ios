93323cfb8edddbebaa7006a45d3d85866c9fc831
Juan Sanzone <juan.sanzone@gmail.com>
2019-04-10 14:25:08 -0300

    Changes

--- MercadoPagoSDK/MercadoPagoSDK/Services/Models/PXCheckoutPreference+AdditionalInfo.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Services/Models/PXCheckoutPreference+AdditionalInfo.swift
@@ -10,11 +10,7 @@ import Foundation
 internal extension PXCheckoutPreference {
     internal func populateAdditionalInfoModel() {
         if let str = self.additionalInfo, let additionInfoData = str.data(using: .utf8) {
-            do {
-                self.pxAdditionalInfo = try JSONDecoder().decode(PXAdditionalInfo.self, from: additionInfoData)
-            } catch {
-                self.pxAdditionalInfo = nil
-            }
+            self.pxAdditionalInfo = try? JSONDecoder().decode(PXAdditionalInfo.self, from: additionInfoData)
         }
     }
 
